e88daf8573c65f3c575a17292abe5ccdb9de3ef1
davi.goncalves <davigoncalves@movida.com.br>
2021-12-13 16:42:24 -0300

    Validação par criação de card

--- BACK/src/api/routes/card.routes.ts
+++ BACK/src/api/routes/card.routes.ts
@@ -1,49 +1,60 @@
-import { CardDTO } from './../dto/card.dto';
+import { CardDTO } from "./../dto/card.dto";
 import express, { Request, Response } from "express";
-import * as cardcontroller from '../controller/card';
+import * as cardcontroller from "../controller/card";
+import { body, validationResult } from "express-validator";
 
 export const cardRouter = express.Router();
 
 cardRouter.get("/", async (req: Request, res: Response) => {
   try {
-    const results = await cardcontroller.getAll()
-    return res.status(200).send(results)
+    const results = await cardcontroller.getAll();
+    return res.status(200).send(results);
   } catch (e: any) {
     res.status(500).send(e.message);
-  } 
+  }
 });
 
-cardRouter.post("/", async (req: Request, res: Response) => {
-  try {
-    const payload:CardDTO = req.body
-    const result = await cardcontroller.create(payload)
-    return res.status(200).send(result)
-  } catch (e: any) {
-    res.status(500).send(e.message);
-  } 
-});
+cardRouter.post(
+  "/",
+  [body("titulo").notEmpty(),
+  body("conteudo").notEmpty()],
+  async (req: Request, res: Response) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+      }
+
+      const payload: CardDTO = req.body;
+      const result = await cardcontroller.create(payload);
+      return res.status(200).send(result);
+    } catch (e: any) {
+      res.status(500).send(e.message);
+    }
+  }
+);
 
 cardRouter.put("/:id", async (req: Request, res: Response) => {
   try {
-    const id = Number(req.params.id)
-    const payload:CardDTO = req.body
+    const id = Number(req.params.id);
+    const payload: CardDTO = req.body;
 
-    const result = await cardcontroller.update(id, payload)
-    return res.status(201).send(result)
+    const result = await cardcontroller.update(id, payload);
+    return res.status(201).send(result);
   } catch (e: any) {
     res.status(500).send(e.message);
-  } 
+  }
 });
 
 cardRouter.delete("/:id", async (req: Request, res: Response) => {
   try {
-    const id = Number(req.params.id)
+    const id = Number(req.params.id);
 
-    const result = await cardcontroller.deleteById(id)
+    const result = await cardcontroller.deleteById(id);
     return res.status(204).send({
-        success: result
-    })
+      success: result,
+    });
   } catch (e: any) {
     res.status(500).send(e.message);
-  } 
+  }
 });
