38f50a6ca5e6a43d39166a9891d6840f87836fb5
davi.goncalves <davigoncalves@movida.com.br>
2021-12-12 22:53:43 -0300

    criando parte de acesso aos dados

---
+++ BACK/src/database/dal/card.ts
@@ -0,0 +1,37 @@
+import {Op} from 'sequelize'
+import Card from '../models/card'
+import { CardInput, CardOuput } from '../models/card'
+
+export const create = async (payload: CardInput): Promise<CardOuput> => {
+  const ingredient = await Card.create(payload)
+  return ingredient
+} 
+
+export const update = async (id: number, payload: Partial<CardInput>): Promise<CardOuput> => {
+  const card = await Card.findByPk(id)
+  if (!card) {
+      // @todo throw custom error
+      throw new Error('not found')
+  }
+  return await (card as Card).update(payload)
+}
+
+export const getById = async (id: number): Promise<CardInput> => {
+  const card = await Card.findByPk(id)
+  if (!card) {
+      // @todo throw custom error
+      throw new Error('not found')
+  }
+  return card
+}
+
+export const deleteById = async (id: number): Promise<boolean> => {
+  const deletedIngredientCount = await Card.destroy({
+      where: {id}
+  })
+  return !!deletedIngredientCount
+}
+
+export const getAll = async (): Promise<CardOuput[]> => {
+  return Card.findAll()
+}
